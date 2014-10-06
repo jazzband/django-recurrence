0a5d2e445b1888e5c80053b661bc5138486300c2
Dominic Rodger <dominicrodger+github@gmail.com>
2014-10-06 17:45:19 +0100

    Switch tests to pytest

--- tests/test_nulls.py
+++ tests/test_nulls.py
@@ -1,17 +1,10 @@
-from django.db import models
-from recurrence.fields import RecurrenceField
+from tests.models import EventWithNulls, EventWithNoNulls
+import pytest
 import unittest
 
 
-class EventWithNoNulls(models.Model):
-    recurs = RecurrenceField(null=False)
-
-
-class EventWithNulls(models.Model):
-    recurs = RecurrenceField(null=True)
-
-
 class RecurrenceTestCase(unittest.TestCase):
+    @pytest.mark.django_db
     def test_recurs_can_be_explicitly_none_if_none_is_allowed(self):
         # Check we can save None correctly
         event = EventWithNulls.objects.create(recurs=None)
@@ -21,6 +14,7 @@ class RecurrenceTestCase(unittest.TestCase):
         reloaded = EventWithNulls.objects.get(pk=event.pk)
         self.assertEqual(reloaded.recurs, None)
 
+    @pytest.mark.django_db
     def test_recurs_cannot_be_explicitly_none_if_none_is_disallowed(self):
         with self.assertRaises(ValueError):
             EventWithNoNulls.objects.create(recurs=None)
