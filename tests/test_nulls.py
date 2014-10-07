fba05401db94d6c3d4ca2d42e5a65d90fd0c6bf7
Dominic Rodger <dominicrodger+github@gmail.com>
2014-10-07 08:01:29 +0100

    Remove use of unittest.TestCase

--- tests/test_nulls.py
+++ tests/test_nulls.py
@@ -1,20 +1,19 @@
 from tests.models import EventWithNulls, EventWithNoNulls
 import pytest
-import unittest
 
 
-class RecurrenceTestCase(unittest.TestCase):
-    @pytest.mark.django_db
-    def test_recurs_can_be_explicitly_none_if_none_is_allowed(self):
-        # Check we can save None correctly
-        event = EventWithNulls.objects.create(recurs=None)
-        self.assertEqual(event.recurs, None)
+@pytest.mark.django_db
+def test_recurs_can_be_explicitly_none_if_none_is_allowed():
+    # Check we can save None correctly
+    event = EventWithNulls.objects.create(recurs=None)
+    assert event.recurs is None
 
-        # Check we can deserialize None correctly
-        reloaded = EventWithNulls.objects.get(pk=event.pk)
-        self.assertEqual(reloaded.recurs, None)
+    # Check we can deserialize None correctly
+    reloaded = EventWithNulls.objects.get(pk=event.pk)
+    assert reloaded.recurs is None
 
-    @pytest.mark.django_db
-    def test_recurs_cannot_be_explicitly_none_if_none_is_disallowed(self):
-        with self.assertRaises(ValueError):
-            EventWithNoNulls.objects.create(recurs=None)
+
+@pytest.mark.django_db
+def test_recurs_cannot_be_explicitly_none_if_none_is_disallowed():
+    with pytest.raises(ValueError):
+        EventWithNoNulls.objects.create(recurs=None)
